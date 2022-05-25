9db6a7f6bcd18291322b6465c4204fb78a1c1ac2
mattdmartin1 <91096561+mattdmartin1@users.noreply.github.com>
2022-05-24 19:58:09 -0700

    second commit

--- hangman.py
+++ hangman.py
@@ -1,3 +1,20 @@
 # hangman
 import random
 from words import words
+
+def get_valid_word(words):
+    word = random.choice(words) # randomly chooses something from the list
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+    
+    return word
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set() # what the user has guessed
+
+
+user_inout = input('Type somthing: ')
+print(user_in)
